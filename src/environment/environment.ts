364e523eb9b7c0b42ebde5be93035e4886623f81
Bhavya Modhiya <bhavyamodhiya7421@gmail.com>
2025-03-20 17:20:48 +0530

    updated backend URL

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    backendUrl: 'http://localhost:3000'  // Your backend URL for development
+  production: true,
+  backendUrl: 'https://low-iq-node.vercel.app',
 };
